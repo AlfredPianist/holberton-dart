1f05d360d19aff71b6a370d79a0552fd564468bb
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-12-11 17:25:19 -0500

    Add: task 2 for 0x01

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,13 @@
+int add(int a, int b) {
+  return a + b;
+}
+
+int sub(int a, int b) {
+  return a - b;
+}
+
+String showFunc(int a, int b) {
+  String str = "Add ${a} + ${b} = ${add(a, b)}\n"
+    "Sub ${a} - ${b} = ${sub(a, b)}";
+  return str;
+}
