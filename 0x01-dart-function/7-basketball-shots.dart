356260bd5c69750f1f857ae8c5dcc31754043024
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-12-11 19:55:47 -0500

    Add: project 7 for 0x01

---
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -0,0 +1,9 @@
+int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
+  int points_team_a = (teamA['Free throws'] ?? 0) +
+    (teamA['2 pointer'] ?? 0) * 2 + (teamA['3 pointer'] ?? 0) * 3;
+  int points_team_b = (teamB['Free throws'] ?? 0) +
+    (teamB['2 pointer'] ?? 0) * 2 + (teamB['3 pointer'] ?? 0) * 3;
+  
+  if (points_team_a == points_team_b) return 0;
+  return points_team_a > points_team_b ? 1 : 2;
+}
