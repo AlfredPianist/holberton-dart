c6548763d48f0cc8f73a34db5467a040f23d4406
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-12-11 20:40:58 -0500

    Add: task 10 for 0x01

---
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -0,0 +1,21 @@
+bool isPalindrome(String s) {
+  if (s.length < 3) return false;
+  String reversed_string = s.split('').reversed.join('');
+  if (s == reversed_string) return true;
+  return false;
+}
+
+String longestPalindrome(String s) {
+  if (s.length < 3) return 'none';
+  String longest = '';
+  String current = '';
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i; j < s.length; j++) {
+      current = s.substring(i, j + 1);
+      if (isPalindrome(current) && current.length > longest.length) {
+        longest = current;
+      }
+    }
+  }
+  return longest != '' ? longest : 'none';
+}
