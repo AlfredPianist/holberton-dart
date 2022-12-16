2a09eed27bfee98a99c2e5cb314dbfb96fe80acb
Alfredo Delgado Moreno <alfredodlgdmrn@gmail.com>
2022-12-16 18:39:04 -0500

    Add: task 5 for 0x02

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,37 @@
+class User {
+  int? id;
+  String? name;
+  int? age;
+  double? height;
+
+  User({required int id, required String name,
+        required int age, required double height}) {
+    this.id = id;
+    this.name = name;
+    this.age = age;
+    this.height = height;
+  }
+
+  Map<String, Object?> toJson () {
+    return {
+      'id': this.id,
+      'name': this.name,
+      'age': this.age,
+      'height': this.height
+    };
+  }
+
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(
+      id: userJson['id'],
+      name: userJson['name'],
+      age: userJson['age'],
+      height: userJson['height']
+    );
+  }
+
+  @override
+  String toString () {
+    return "User(id : ${this.id} ,name: ${this.name}, age: ${this.age}, height: ${this.height})";
+  }
+}
